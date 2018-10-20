c6af4433f6773584f9559749325758679b9c64a4
Thales César <thalesgiriboni@gmail.com>
2018-10-20 00:08:35 -0300

    Finish POC of CamDep SDK and first POC of app

--- poc.py
+++ poc.py
@@ -7,30 +7,40 @@ class APIData():
 
     def __init__(self, api_section):
         self.http = urllib3.PoolManager()
-        self.api_section = api_section
+        self._api_section = api_section
 
-    def runThroughAllPages(self, **kwargs):
+    def runThroughAllPages(self, *args, **kwargs):
         data_length = None
         page_num = 1
         url_args = kwargs
+        if len(args):
+            url_path = '{}{}/{}{}'.format(
+                camara_dos_deputados_endpoint,
+                self._api_section,
+                args[0],
+                '/{}'.format(args[1]) if len(args) > 1 else ''
+            )
+        else:
+            url_path = '{}{}'.format(camara_dos_deputados_endpoint, self._api_section)
         while data_length != 0:
             url_args['pagina'] = str(page_num)
             r = self.http.request(
                 'GET',
-                '{}{}'.format(camara_dos_deputados_endpoint, self.api_section),
+                url_path,
                 fields=url_args,
                 headers={'accept': 'application/json'}
             )
             if r.status != 200:
-                raise Exception('API call failed')
+                raise Exception('API call failed: {}'.format(url_args))
             
             try:
                 page = json.loads(r.data.decode('utf-8'))
                 data_length = len(page['dados'])
-                yield page['dados']
-                page_num += 1
+                dados = page['dados']
             except:
                 raise Exception('Failed to decode API data')
+            yield dados
+            page_num += 1
 
     def getAPISingleRequest(self, param_id, param_page = ''):
         if param_page:
@@ -39,14 +49,14 @@ class APIData():
             'GET',
             '{}{}/{}{}'.format(
                 camara_dos_deputados_endpoint,
-                self.api_section,
+                self._api_section,
                 param_id,
                 param_page
             ),
             headers={'accept': 'application/json'}
         )
         if r.status != 200:
-            raise Exception('API call failed')
+            raise Exception('API call failed: {}'.format(param_id))
         try:
             return json.loads(r.data.decode('utf-8'))['dados']
         except:
@@ -104,16 +114,36 @@ class Votacoes(APIData):
 
     def obterVotacao(self, vot_id):
         return self.getAPISingleRequest(vot_id)
+
+    def obterVotos(self, vot_id, **kwargs):
+        return self.runThroughAllPages(vot_id, 'votos', **kwargs)
+
+class Eventos(APIData):
+
+    def __init__(self):
+        super().__init__('eventos')
+
+    def obterTodosEventos(self, **kwargs):
+        return self.runThroughAllPages(**kwargs)
+
+    def obterEvento(self, ev_id):
+        return self.getAPISingleRequest(ev_id)
+
+    def obterDeputadosEvento(self, ev_id):
+        return self.getAPISingleRequest(ev_id, 'deputados')
+
+    def obterPautaEvento(self, ev_id):
+        return self.getAPISingleRequest(ev_id, 'pauta')
             
 
 dep = Deputados()
 prop = Proposicoes()
 vot = Votacoes()
+ev = Eventos()
 
 """ IDs de exemplo:
     Deputado: 74171
     Proposição: 2129185
     Votação: 8127, 8122
+    Evento: 53848
 """
-
-print(vot.obterVotacao(8127))
---
+++ poc_app.py
@@ -0,0 +1,70 @@
+from datetime import timedelta, datetime
+from poc import Deputados, Eventos, Proposicoes, Votacoes
+
+
+def procurarDeputado(nome_deputado):
+    dep = Deputados()
+    for page in dep.obterTodosDeputados():
+        for item in page:
+            if item['nome'].lower() == nome_deputado.lower():
+                return item
+
+
+def procurarEventosComDeputado(deputado_id):
+    ev = Eventos()
+    eventos_com_deputado = []
+    eventos_totais = 0
+    agora =  datetime.now() #datetime(2018, 6, 26)
+    last_week = agora - timedelta(weeks=1)
+    for page in ev.obterTodosEventos(
+        dataInicio='{}-{:02d}-{:02d}'.format(last_week.year,
+                                     last_week.month, last_week.day),
+        dataFim='{}-{:02d}-{:02d}'.format(agora.year,
+                                  agora.month, agora.day)
+    ):
+        for item in page:
+            for dep in ev.obterDeputadosEvento(item['id']):
+                eventos_totais += 1
+                if dep['id'] == deputado_id:
+                    eventos_com_deputado.append(item)
+    print('Presença: {0:.2f}%'.format(100*len(eventos_com_deputado)/eventos_totais))
+    return eventos_com_deputado
+
+
+def obterPautaEvento(ev_id):
+    ev = Eventos()
+    prop = Proposicoes()
+    pauta = ev.obterPautaEvento(ev_id)
+    if not pauta:
+        return None
+    proposicao_id = pauta[0]['proposicao_']['id']
+    if proposicao_id:
+        proposicao_detalhes = prop.obterProposicao(proposicao_id)
+        votacao = prop.obterVotacoesProposicao(proposicao_id)
+        return (proposicao_detalhes, votacao)
+
+
+def obterVotoDeputado(vot_id, dep_id):
+    vot = Votacoes()
+    for page in vot.obterVotos(vot_id):
+        for v in page:
+            if v['parlamentar']['id'] == dep_id:
+                return v['voto']
+
+
+def main(*args, **kwargs):
+    resp = input('Inserir nome de Deputado: ')
+    deputado = procurarDeputado(resp)
+    print(deputado)
+    eventos = procurarEventosComDeputado(deputado['id'])
+    for e in eventos:
+        pauta = obterPautaEvento(e['id'])
+        if pauta and pauta[1]:
+            print('Votou {} em {}'.format(
+                obterVotoDeputado(pauta[1][0]['id'], deputado['id']),
+                pauta[0]['ementa']
+            ))
+
+
+if __name__ == '__main__':
+    main()
